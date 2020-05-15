16e2650718d45830a1098721e25d1197ff8a3dbc
tobilarscheid <tobilarscheid@gmail.com>
2020-05-15 09:34:31 +0200

    add md5 hash

--- src/main/kotlin/com/larscheid/bloomfilter/hash/Hashes.kt
+++ src/main/kotlin/com/larscheid/bloomfilter/hash/Hashes.kt
@@ -1,5 +1,8 @@
 package com.larscheid.bloomfilter.hash
 
+import java.nio.ByteBuffer
+import java.security.MessageDigest
+
 /**
  * Returns a hash function for a given name
  *
@@ -7,9 +10,15 @@ package com.larscheid.bloomfilter.hash
  */
 fun hashForName(name: String) : (String) -> Int = when (name) {
     "kotlin" -> ::kotlinDefaultHash
-    else -> throw UnknownHashException("No implementation for hash $name found")
+    "md5" -> ::md5Hash
+    else -> throw UnknownHashException("No implementation for hash $name found - please check your configuration")
 }
 
 fun kotlinDefaultHash(word: String) : Int = word.hashCode()
+
+fun md5Hash(word: String) : Int {
+   val hashBytes = MessageDigest.getInstance("MD5").digest(word.toByteArray())
+    return ByteBuffer.wrap(hashBytes).int
+}
 
 class UnknownHashException(message: String) : Exception(message)
